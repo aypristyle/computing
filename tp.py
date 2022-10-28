082c0c4941bf3e6cc968968e4da1d2aabfb2ea2a
aypristyle <58827391+aypristyle@users.noreply.github.com>
2022-10-28 19:18:38 +0200

    Update tp.py
    add examples

--- tp.py
+++ tp.py
@@ -3,7 +3,7 @@ def the_sequence(n):
         :param n (int): the number of terms that we want to return
         :return res (list) the list of the n firts term of the sequence"""
     res = ["1"]
-    for i in range(n):
+    for i in range(n-1):
         res.append(next_term(res[i]))
     return res
 
@@ -22,6 +22,7 @@ def is_valid_sudoku(board):
 
 
 def next_term(a):
+    """auxiliary function who compute the next term of the sequence"""
     temp = 0
     count = 0
     res = []
@@ -45,6 +46,52 @@ def is_valid_tictactoe(board):
     """Return if the tictactoe board is good or not
         :param board (list) the list containing the tictactoe board
         :return res (bool) if the board is good or not"""
+    X = 0
+    O = 0
+    for line in board:
+        X = X + line.count("X")
+        O = O + line.count("O")
+    return X >= O and X - O <= 1
 
 
-print(the_sequence(6))
+print(is_valid_tictactoe(["XO ", " X ", " "]))
+
+print(the_sequence(1))
+
+
+def test():
+    valid_board = [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+                , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+                , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+                , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+                , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+                , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+                , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+                , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+                , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+
+    invalid_board_col = [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+                , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+                , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+                , ["6", ".", ".", ".", "6", ".", ".", ".", "3"]
+                , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+                , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+                , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+                , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+                , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+
+    invalid_board_line = [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+                , ["6", ".", ".", "1", "6", "5", ".", ".", "."]
+                , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+                , ["5", ".", ".", ".", "6", ".", ".", ".", "3"]
+                , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+                , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+                , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+                , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+                , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+    tictactoe_valid = ["XO ", " X ", " "]
+    tictactoe_not_valid = ["XOX", " X ", " "]
+    tictactoe_invalid = ["XOO", " X ", " "]
+
+
+
